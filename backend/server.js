bbd4f7afc7f7a2dea7b6c69f7329a4c3ba853b18
winthawdaraung <Wtda2426@gmail.com>
2025-01-31 01:52:26 +0700

    removed nodemodules

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 import productRouter from "./routes/product.route.js";
 dotenv.config();
@@ -8,7 +9,19 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const __dirname = path.resolve();
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) => {    
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running...");
+    });
+}
 
 app.use("/api/products", productRouter);
 app.listen(PORT, () => {
